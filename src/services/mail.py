998a13377a11bb7c017b0533e55b9da29185c393
ADENIYI OLANREWAJU <100721103+engrmarkk@users.noreply.github.com>
2025-05-28 12:03:16 +0100

    services (mail) smtp

---
+++ src/services/mail.py
@@ -0,0 +1,21 @@
+from flask import render_template
+from flask_mail import Message
+from logger import logger
+
+from src.constants.env_constant import SENDER_EMAIL, SENDER_NAME
+from src.extentions.extensions import mail
+
+
+def send_mail(data):
+    try:
+        msg = Message(
+            subject=data["subject"],  # Dynamic subject passed to the function
+            sender=f"{SENDER_NAME} <{SENDER_EMAIL}>",
+            recipients=[data["email"]],
+        )
+        msg.html = render_template(data["template"], **data)
+        mail.send(msg)
+        return True
+    except Exception as e:
+        logger.error(f"Error sending email: {e}")
+        return False
