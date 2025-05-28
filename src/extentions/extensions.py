2e13a57806b7ec8aad2d9d9ac3b211a1645013c9
ADENIYI OLANREWAJU <100721103+engrmarkk@users.noreply.github.com>
2025-05-28 12:02:57 +0100

    Update extensions.py

--- src/extentions/extensions.py
+++ src/extentions/extensions.py
@@ -4,14 +4,15 @@ from sqlalchemy import MetaData
 from flask_migrate import Migrate
 from flask_cors import CORS
 from flask_mail import Mail
+
 # from flask_socketio import SocketIO
 
 naming_convention = {
-    "ix": 'ix_%(column_0_label)s',
+    "ix": "ix_%(column_0_label)s",
     "uq": "uq_%(table_name)s_%(column_0_name)s",
     "ck": "ck_%(table_name)s_%(column_0_name)s",
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-    "pk": "pk_%(table_name)s"
+    "pk": "pk_%(table_name)s",
 }
 
 
