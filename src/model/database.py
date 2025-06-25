bd3dca9a408eb4073c82d3ceabe7adcc268e2f6a
ADENIYI OLANREWAJU <100721103+engrmarkk@users.noreply.github.com>
2025-06-25 23:07:42 +0100

    Update database.py

--- src/model/database.py
+++ src/model/database.py
@@ -77,24 +77,26 @@ class Products(db.Model):
     out_of_stock = db.Column(db.Boolean, default=False)
     category_id = db.Column(db.String(50), db.ForeignKey("category.id"), nullable=False)
     image = db.Column(db.String(500), nullable=False)
-    specification_1 = db.Column(db.String(100), nullable=True)
-    specification_2 = db.Column(db.String(100), nullable=True)
-    specification_3 = db.Column(db.String(100), nullable=True)
-    specification_4 = db.Column(db.String(100), nullable=True)
-    specification_5 = db.Column(db.String(100), nullable=True)
-    specification_6 = db.Column(db.String(100), nullable=True)
-    specification_7 = db.Column(db.String(100), nullable=True)
-    specification_8 = db.Column(db.String(100), nullable=True)
-    specification_9 = db.Column(db.String(100), nullable=True)
-    specification_10 = db.Column(db.String(100), nullable=True)
-    specification_11 = db.Column(db.String(100), nullable=True)
-    specification_12 = db.Column(db.String(100), nullable=True)
-    specification_13 = db.Column(db.String(100), nullable=True)
-    specification_14 = db.Column(db.String(100), nullable=True)
-    specification_15 = db.Column(db.String(100), nullable=True)
+    # specification_1 = db.Column(db.String(100), nullable=True)
+    # specification_2 = db.Column(db.String(100), nullable=True)
+    # specification_3 = db.Column(db.String(100), nullable=True)
+    # specification_4 = db.Column(db.String(100), nullable=True)
+    # specification_5 = db.Column(db.String(100), nullable=True)
+    # specification_6 = db.Column(db.String(100), nullable=True)
+    # specification_7 = db.Column(db.String(100), nullable=True)
+    # specification_8 = db.Column(db.String(100), nullable=True)
+    # specification_9 = db.Column(db.String(100), nullable=True)
+    # specification_10 = db.Column(db.String(100), nullable=True)
+    # specification_11 = db.Column(db.String(100), nullable=True)
+    # specification_12 = db.Column(db.String(100), nullable=True)
+    # specification_13 = db.Column(db.String(100), nullable=True)
+    # specification_14 = db.Column(db.String(100), nullable=True)
+    # specification_15 = db.Column(db.String(100), nullable=True)
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
     category = db.relationship("Category", backref="products")
+    specification = db.relationship("Specification", backref="products")
+    product_images = db.relationship("ProductImages", backref="products")
 
     def to_dict(self):
         return {
@@ -108,23 +110,44 @@ class Products(db.Model):
             "category_id": self.category_id,
             "category_name": self.category.name,
             "image": self.image,
-            "specification_1": self.specification_1,
-            "specification_2": self.specification_2,
-            "specification_3": self.specification_3,
-            "specification_4": self.specification_4,
-            "specification_5": self.specification_5,
-            "specification_6": self.specification_6,
-            "specification_7": self.specification_7,
-            "specification_8": self.specification_8,
-            "specification_9": self.specification_9,
-            "specification_10": self.specification_10,
-            "specification_11": self.specification_11,
-            "specification_12": self.specification_12,
-            "specification_13": self.specification_13,
-            "specification_14": self.specification_14,
-            "specification_15": self.specification_15,
+            # "specification_1": self.specification_1,
+            # "specification_2": self.specification_2,
+            # "specification_3": self.specification_3,
+            # "specification_4": self.specification_4,
+            # "specification_5": self.specification_5,
+            # "specification_6": self.specification_6,
+            # "specification_7": self.specification_7,
+            # "specification_8": self.specification_8,
+            # "specification_9": self.specification_9,
+            # "specification_10": self.specification_10,
+            # "specification_11": self.specification_11,
+            # "specification_12": self.specification_12,
+            # "specification_13": self.specification_13,
+            # "specification_14": self.specification_14,
+            # "specification_15": self.specification_15,
             "created_at": self.created_at.isoformat() if self.created_at else None,
         }
+
+
+# product images
+class ProductImages(db.Model):
+    __tablename__ = "product_images"
+    id = db.Column(db.String(50), primary_key=True, default=random_id)
+    image = db.Column(db.Text, nullable=False)
+    product_id = db.Column(db.String(50), db.ForeignKey("products.id"), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    
+
+class Specification(db.Model):
+    __tablename__ = "specification"
+    id = db.Column(db.String(50), primary_key=True, default=random_id)
+    name = db.Column(db.String(100), nullable=True)
+    description = db.Column(db.Text, nullable=True)
+    product_id = db.Column(db.String(50), db.ForeignKey("products.id"), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def to_dict(self):
+        return {"id": self.id, "name": self.name}
 
 
 class Cart(db.Model):
