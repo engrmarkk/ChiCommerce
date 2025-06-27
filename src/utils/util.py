f62e9be87f7b925b2eb147cc1a09859aa552dac2
ADENIYI OLANREWAJU <100721103+engrmarkk@users.noreply.github.com>
2025-06-27 14:19:50 +0100

    Update util.py

--- src/utils/util.py
+++ src/utils/util.py
@@ -11,6 +11,8 @@ from passlib.hash import pbkdf2_sha256 as sha256
 
 from src.constants.env_constant import ACCESS_TOKEN_EXPIRES
 from src.logger import logger
+from src.connections.redis_connection import redis_conn
+import json
 
 
 def return_response(status_code, status=None, message=None, **data):
@@ -54,3 +56,30 @@ def generate_signature(params_to_sign, api_secret):
 
 def format_datetime(datetime_obj, format="%Y-%m-%d %H:%M:%S"):
     return datetime_obj.strftime(format) if datetime_obj else None
+
+
+# return data if exists in redis else return the data after caching
+def data_cache(key, data, expire=None):
+    try:
+        data_cache = redis_conn.get(key)
+        if data_cache:
+            logger.info(f"data found in redis for key: {key}")
+            return json.loads(data_cache)
+        else:
+            if expire:
+                logger.info(f"data not found in redis for key: {key}")
+                redis_conn.set(key, json.dumps(data), expire)
+            else:
+                redis_conn.set(key, json.dumps(data))
+            logger.info(f"data saved in redis for key: {key}")
+            return data
+    except Exception as e:
+        logger.exception(e)
+        return None
+
+
+def return_host_url(host_url):
+    # if host url starts with http instead of https
+    if host_url.startswith("http://"):
+        host_url = host_url.replace("http://", "https://")
+    return host_url
