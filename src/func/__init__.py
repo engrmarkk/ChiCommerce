28b1e0af3dd90454844b74204e8b5d2674c5f801
ADENIYI OLANREWAJU <100721103+engrmarkk@users.noreply.github.com>
2025-07-29 12:10:53 +0100

    Add cart and transaction management functions
    Introduces functions for cart operations including adding items, retrieving by reference ID, finalizing, clearing, and processing payments. Also adds transaction and product purchase saving logic, and imports required models and logger.

--- src/func/__init__.py
+++ src/func/__init__.py
@@ -1,4 +1,19 @@
-from src.model.database import Specification, Products, db, ProductImages
+from src.model.database import (
+    db,
+    Specification,
+    Products,
+    ProductImages,
+    Cart,
+    Transaction,
+    ProductPurchased,
+)
+import uuid
+from src.logger import logger
+
+
+# generate cart_ref_id
+def generate_cart_ref_id():
+    return str(uuid.uuid4().hex)
 
 
 def update_product_specifications(product_id, specifications):
@@ -48,3 +63,107 @@ def update_product_images(product_id, images):
             db.session.add(image_obj)
     db.session.commit()
     return product
+
+
+# add to cart
+def add_to_cart(user_id, product_id, quantity, cart_ref_id):
+    try:
+        cart_item = Cart(
+            user_id=user_id,
+            product_id=product_id,
+            quantity=quantity,
+            cart_ref_id=cart_ref_id,
+        )
+        db.session.add(cart_item)
+        db.session.commit()
+        return cart_item
+    except Exception as e:
+        db.session.rollback()
+        logger.exception(f"Error adding to cart: {e}")
+        return None
+
+
+# get cart items by ref_id
+def get_cart_items_by_ref_id(cart_ref_id, user_id):
+    try:
+        cart_items = Cart.query.filter_by(
+            cart_ref_id=cart_ref_id, user_id=user_id
+        ).all()
+        return [item.to_dict() for item in cart_items]
+    except Exception as e:
+        logger.exception(f"Error retrieving cart items: {e}")
+        return []
+
+
+def finalize_cart_item(products, user_id):
+    try:
+        cart_ref_id = generate_cart_ref_id()
+        for product in products:
+            # if product_id and quantity, add to cart directly
+            if product.get("product_id") and product.get("quantity"):
+                cart_item = add_to_cart(
+                    user_id=user_id,
+                    product_id=product["product_id"],
+                    quantity=product["quantity"],
+                    cart_ref_id=cart_ref_id,
+                )
+        return get_cart_items_by_ref_id(cart_ref_id, user_id)
+    except Exception as e:
+        logger.exception(f"Error finalizing cart item: {e}")
+        return None
+
+
+def clear_cart(user_id, cart_ref_id):
+    try:
+        cart_items = get_cart_items_by_ref_id(cart_ref_id, user_id)
+        for item in cart_items:
+            db.session.delete(item)
+        db.session.commit()
+        return True
+    except Exception as e:
+        db.session.rollback()
+        logger.exception(f"Error clearing cart: {e}")
+        return None
+
+
+def process_cart_payment(user_id, data, cart_ref_id):
+    carts_items = get_cart_items_by_ref_id(cart_ref_id, user_id)
+    trans = save_transaction(data, user_id)
+    for cart in carts_items:
+        save_product_purchased(
+            cart.product_id,
+            user_id,
+            cart.quantity * cart.product.price,
+            trans.id,
+            cart.quantity,
+        )
+        # reduce_land_plot(cart.property_id, cart.count)
+    return
+
+
+def save_transaction(data, user_id):
+    new_trans = Transaction(
+        amount=float(data.get("amount")) / 100,
+        user_id=user_id,
+        channel=data.get("channel"),
+        transaction_id=data.get("id"),
+        authorization_dict=data.get("authorization", {}),
+        ip_address=data.get("ip_address"),
+        reference_number=data.get("reference"),
+    )
+    db.session.add(new_trans)
+    db.session.commit()
+    return new_trans
+
+
+def save_product_purchased(product_id, user_id, amount, transaction_id, quantity):
+    new_purchase = ProductPurchased(
+        product_id=product_id,
+        user_id=user_id,
+        amount=amount,
+        transaction_id=transaction_id,
+        quantity=quantity,
+    )
+    db.session.add(new_purchase)
+    db.session.commit()
+    return new_purchase
