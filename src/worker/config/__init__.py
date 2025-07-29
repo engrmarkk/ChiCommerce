6efc7e73eaabbdf91da218e2a4765c4d2a667add
ADENIYI OLANREWAJU <100721103+engrmarkk@users.noreply.github.com>
2025-07-29 12:11:22 +0100

    Update Celery config for SSL and logger import
    Changed logger import to use src.logger and updated Celery backend and broker URLs to include ssl_cert_reqs=CERT_NONE for SSL configuration.

--- src/worker/config/__init__.py
+++ src/worker/config/__init__.py
@@ -2,7 +2,7 @@ from celery import Celery, shared_task
 from src import create_app
 from src.constants.env_constant import REDIS_URL
 from dotenv import load_dotenv
-from logger import logger
+from src.logger import logger
 import src.worker.schedule as celeryConfig
 
 load_dotenv()
@@ -17,8 +17,8 @@ def make_celery(app=app):
     """
     celery = Celery(
         app.import_name,
-        backend=REDIS_URL,
-        broker=REDIS_URL,
+        backend=f"{REDIS_URL}?ssl_cert_reqs=CERT_NONE",
+        broker=f"{REDIS_URL}?ssl_cert_reqs=CERT_NONE",
     )
     celery.conf.update(app.config)
     celery.config_from_object(celeryConfig)
