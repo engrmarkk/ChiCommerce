43023b98be748c8dea9e8ffc1798a43315e7da4a
ADENIYI OLANREWAJU <100721103+engrmarkk@users.noreply.github.com>
2025-08-20 11:55:54 +0100

    Update monnify_webhooks.py

--- src/users/webhooks/monnify_webhooks.py
+++ src/users/webhooks/monnify_webhooks.py
@@ -1,15 +1,30 @@
 from . import webhooks_blp
 from src.logger import logger
-from src.utils.util import return_response
+from src.utils.util import return_response, compute_transaction_hash
 from src.constants import http_status_codes
 from src.constants.status_message import StatusMessage
 from src.constants.env_constant import EXCEPTION_MESSAGE
+from flask import request
 
 MONNIFY_PREFIX = "monnify"
 
-@webhooks_blp.get(f"/{MONNIFY_PREFIX}/payment_successful")
+
+@webhooks_blp.post(f"/{MONNIFY_PREFIX}/payment_successful")
 def monnify_webhooks():
     try:
+        raw_payload = request.get_data()
+        signature = request.headers.get("monnify-signature")
+        computed = compute_transaction_hash(raw_payload)
+        logger.info(f"computed: {computed}")
+        logger.info(f"signature: {signature}")
+        if signature != computed:
+            logger.info("Invalid signature")
+            return return_response(
+                http_status_codes.HTTP_403_FORBIDDEN,
+                status=StatusMessage.FAILED,
+                message="Invalid signature",
+            )
+        logger.info("Valid signature")
         return return_response(
             http_status_codes.HTTP_200_OK,
             status=StatusMessage.SUCCESS,
